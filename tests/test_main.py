69a3b96cb555de4dc62780dfe27136f57d08c322
MaksymTer <maksymteresa@gmail.com>
2025-06-11 16:41:35 +0200

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,58 +1,40 @@
-from io import StringIO
-import sys
+from typing import Type
 
-import pytest
-
-from app.main import main, Book
-
-
-def get_stdout(func, *args, **kwargs) -> str:
-    new_stdout = StringIO()
-    old_stdout = sys.stdout
-    sys.stdout = new_stdout
-
-    func(*args, **kwargs)
-
-    sys.stdout = old_stdout
-    return new_stdout.getvalue()
+from book import Book
+from displays import Display, ConsoleDisplay, ReverseDisplay
+from serializers import Serializer, JsonSerializer, XmlSerializer
+from printers import Printer, ConsolePrinter, ReversePrinter
 
 
-@pytest.fixture()
-def book() -> Book:
-    return Book("Sample Book", "This is some sample content.")
+DISPLAY_STRATEGIES: dict[str, Type[Display]] = {
+    "console": ConsoleDisplay,
+    "reverse": ReverseDisplay,
+}
+
+PRINT_STRATEGIES: dict[str, Type[Printer]] = {
+    "console": ConsolePrinter,
+    "reverse": ReversePrinter,
+}
+
+SERIALIZER_STRATEGIES: dict[str, Type[Serializer]] = {
+    "json": JsonSerializer,
+    "xml": XmlSerializer,
+}
 
 
-def test_display_console(book) -> None:
-    output = get_stdout(main, book, [("display", "console")])
-    assert "This is some sample content." in output
+def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
+    for cmd, method_type in commands:
+        if cmd == "display":
+            DISPLAY_STRATEGIES[method_type]().display(book.content)
+
+        elif cmd == "print":
+            PRINT_STRATEGIES[method_type]().print_book(book)
+
+        elif cmd == "serialize":
+            return SERIALIZER_STRATEGIES[method_type]().serialize(book)
 
 
-def test_display_reverse(book) -> None:
-    output = get_stdout(main, book, [("display", "reverse")])
-    assert "tnetnoc elpmas emos si sihT" in output
+if __name__ == "__main__":
+    sample_book = Book("Sample Book", "This is some sample content.")
+    print(main(sample_book, [("display", "reverse"), ("serialize", "xml")]))
 
-
-def test_print_console(book) -> None:
-    output = get_stdout(main, book, [("print", "console")])
-    assert book.title in output
-    assert "This is some sample content." in output
-
-
-def test_print_reverse(book) -> None:
-    output = get_stdout(main, book, [("print", "reverse")])
-    assert book.title in output
-    assert "tnetnoc elpmas emos si sihT" in output
-
-
-def test_serialize_json(book) -> None:
-    serialized_book = main(book, [("serialize", "json")])
-    assert (
-        serialized_book
-        == '{"title": "Sample Book", "content": "This is some sample content."}'
-    )
-
-
-def test_serialize_xml(book) -> None:
-    serialized_book = main(book, [("serialize", "xml")])
-    assert "<title>Sample Book</title>" in serialized_book
-    assert "<content>This is some sample content.</content>" in serialized_book
